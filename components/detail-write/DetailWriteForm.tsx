21787d33aae3b62a4c623a12965a5e72c14ef8e3
Jeong <hj08hn@gmail.com>
2023-02-17 19:21:02 +0900

    ✨Feat: create post form #26
    - 글쓰기 페이지 폼을 만듭니다

--- components/detail-write/DetailWriteForm.tsx
+++ components/detail-write/DetailWriteForm.tsx
@@ -2,9 +2,12 @@ import React, { useState } from 'react';
 import styled from 'styled-components';
 import dynamic from 'next/dynamic';
 import DetailWriteSearch from './DetailWriteSearch';
+import DetailWriteProductCard from './DetailWriteProductCard';
+
 type Props = {};
 
-const DetailWriteForm = (props: any) => {
+// 글쓰기 페이지 폼 함수입니다
+const DetailWriteForm = () => {
   const Editor = dynamic(() => import('./DetailWriteFormEditor'), {
     ssr: false,
   });
@@ -21,29 +24,42 @@ const DetailWriteForm = (props: any) => {
 
   return (
     <>
-      <DetailWriteFormLayout>
+      <DetailWriteLayout>
         {isModalShow && <DetailWriteSearch onClose={hideSearchModal} />}
-        <div>
-          <label>제목</label>
-          <input />
-        </div>
-        <div>
-          <select>
-            <option value="select">선택해주세요</option>
+        <DetailWriteSelectBox>
+          <select className="job_select">
+            <option value="select" className="optionOne">
+              선택해주세요
+            </option>
             <option value="개발자">개발자</option>
             <option value="디자이너">디자이너</option>
             <option value="기획자">기획자</option>
             <option value="학생">학생</option>
             <option value="기타">기타</option>
           </select>
-          <span>의 책상</span>
-        </div>
+          <p className="job_span">의 책상</p>
+        </DetailWriteSelectBox>
+        <DetailWriteTitleInput
+          type="text"
+          placeholder="제목을 입력해주세요"
+          maxLength={30}
+        />
         <Editor />
-        <DetailWriteFormButtonBox>
-          <button>취소</button>
-          <button>완료</button>
-        </DetailWriteFormButtonBox>
-      </DetailWriteFormLayout>
+        <DetailWriteBox>
+          <span className="title_span">
+            테스크테리어 분위기 색상을 선택해주세요
+          </span>
+        </DetailWriteBox>
+        <DetailWriteBox>
+          <span className="title_span">사용하신 제품을 선택해주세요</span>
+          <DetailWriteProductCard />
+        </DetailWriteBox>
+        <DetailWriteButtonBox>
+          <button className="btn">취소</button>
+          <button className="btn">완료</button>
+        </DetailWriteButtonBox>
+      </DetailWriteLayout>
+
       <button onClick={showSearchModal}>기기검색</button>
     </>
   );
@@ -51,16 +67,88 @@ const DetailWriteForm = (props: any) => {
 
 export default DetailWriteForm;
 
-const DetailWriteFormLayout = styled.form`
+const DetailWriteLayout = styled.form`
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  background-color: yellow;
   margin: 0 auto;
   width: 80%;
+  height: 100vh;
+  padding: 0.5rem;
 `;
 
-const DetailWriteFormButtonBox = styled.div`
+const DetailWriteSelectBox = styled.div`
   display: flex;
+  align-items: center;
+  width: 75rem;
+  height: 3.25rem;
+
+  .job_select {
+    width: 7.375rem;
+    height: 1.75rem;
+    border: 0.0625rem solid #868e96;
+    border-radius: 0.625rem;
+    padding: 0.25rem 0.5rem;
+
+    &:hover {
+      border-color: #206efb;
+    }
+  }
+
+  .job_span {
+    font-size: 1.125rem;
+    line-height: 1.25rem;
+  }
+`;
+
+const DetailWriteTitleInput = styled.input`
+  display: flex;
+  width: 75rem;
+  height: 3.25rem;
+  box-sizing: border-box;
+  border: 0.125rem solid #868e96;
+  border-radius: 0.625rem;
+  padding: 0.875rem 1.25rem;
+  font-size: 1.375rem;
+`;
+
+const DetailWriteButtonBox = styled.div`
+  display: flex;
+  justify-content: end;
+  width: 75rem;
+  height: 3.25rem;
+
+  .btn {
+    background-color: #206efb;
+    border: none;
+    border-radius: 0.625rem;
+    color: white;
+    font-size: 1.25rem;
+    line-height: 1.25rem;
+    width: 9.375rem;
+    height: 3.25rem;
+    margin: 0 0.8rem;
+    padding: 1rem 2.5rem;
+
+    &:hover {
+      color: #206efb;
+      background-color: #fff;
+    }
+  }
+`;
+
+const DetailWriteBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 75rem;
+  height: 3.25rem;
+  margin: 2rem;
+
+  .title_span {
+    margin-bottom: 0.625rem;
+    font-weight: 700;
+    font-size: 1.5rem;
+    line-height: 2rem;
+  }
 `;
