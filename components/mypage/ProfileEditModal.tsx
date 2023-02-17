e253ce6e25e19676ecfeabe1be503e502f8ea14c
HongguLim <Honggu.Lim@outlook.com>
2023-02-17 19:57:42 +0900

    ✨: 닉네임 정보수정 기능 구현, 내정보 수정 모달 스위치구현 #35 #37

--- components/mypage/ProfileEditModal.tsx
+++ components/mypage/ProfileEditModal.tsx
@@ -1,7 +1,10 @@
+import { app, auth } from '@/shared/firebase';
+import { updateProfile } from 'firebase/auth';
 import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 
 function ProfileEditModal({ setProfileEditModalOpen, user }: any) {
+  const [toggleEditMenu, setToggleEditMenu] = useState(true);
   const [nickNameEditEnable, setNickNameEditEnable] = useState(true);
   const [nickNameEdit, setNickNameEdit] = useState('');
   const [countCharacters, setCountCharacters] = useState(0);
@@ -28,6 +31,7 @@ function ProfileEditModal({ setProfileEditModalOpen, user }: any) {
 
   const nickNameEditonChangeHandler = function (event: any) {
     const inputText = event.target.value;
+
     if (!inputText) {
       setNickNameEdit(user.displayName);
     } else {
@@ -35,6 +39,32 @@ function ProfileEditModal({ setProfileEditModalOpen, user }: any) {
     }
   };
 
+  const confirmButtonHandler = function () {
+    const regex = /^[\w\Wㄱ-ㅎㅏ-ㅣ가-힣]{2,8}$/;
+    if (regex.test(nickNameEdit)) {
+      updateProfile(auth.currentUser as any, {
+        displayName: nickNameEdit,
+      });
+    } else {
+      alert(
+        '닉네임은 특수문자를 포함할수 없고 2글자 이상 8자이하이어야합니다.',
+      );
+    }
+  };
+
+  // const confirmButtonHandler = function () {
+  //   const regex =
+  //     /^(?=.*[a-zA-z])(?=.*[0-9])(?=.*[$`~!@$!%*#^?&\\(\\)\-_=+])(?!.*[^a-zA-z0-9$`~!@$!%*#^?&\\(\\)\-_=+]).{8,16}$/;
+  //   if (regex.test(nickNameEdit)) {
+  //     updateProfile(auth.currentUser as any, {
+  //       displayName: nickNameEdit,
+  //     });
+  //   } else {
+  //     alert(
+  //       '비밀번호는 8~16자리 / 영문 대소문자, 숫자, 특수문자 포함이어야합니다.',
+  //     );
+  //   }
+  // };
   return (
     <StyledEditProfileModalContainer>
       <div
@@ -53,15 +83,17 @@ function ProfileEditModal({ setProfileEditModalOpen, user }: any) {
             />
           </div>
           <div>
-            <ul>
-              <li> 프로필 정보 설정</li>
-              <li>비밀번호 변경</li>
-            </ul>
+            <button onClick={() => setToggleEditMenu(true)}>
+              프로필 정보 설정
+            </button>
+            <button onClick={() => setToggleEditMenu(false)}>
+              비밀번호 변경
+            </button>
           </div>
         </div>
         <div className="right-continer">
           <div>
-            <p>프로필 정보 설정</p>
+            <p>{toggleEditMenu ? '프로필 정보 설정' : '비밀번호 변경'}</p>
             <button
               className="modal-close"
               onClick={() => setProfileEditModalOpen(false)}
@@ -69,26 +101,51 @@ function ProfileEditModal({ setProfileEditModalOpen, user }: any) {
               X
             </button>
           </div>
-          <div>닉네임</div>
 
-          <input
-            type="text"
-            placeholder={user.displayName}
-            disabled={nickNameEditEnable}
-            onChange={nickNameEditonChangeHandler}
-            value={nickNameEdit}
-          />
-          <button onClick={nickNameEditHandler}>닉네임수정</button>
-          <div>프로필소개</div>
-          <input
-            type="text"
-            placeholder="프로필 소개 들어감"
-            onChange={countOnChangeHandler}
-            value={Characters}
-          />
-          <p>{countCharacters}/50</p>
-          <p> 프로필 정보를 성공적으로 변경하였습니다.</p>
-          <button>적용하기</button>
+          {toggleEditMenu ? (
+            <div className="ProfileEdit">
+              <div>닉네임</div>
+              <input
+                type="text"
+                placeholder={user.displayName}
+                disabled={nickNameEditEnable}
+                onChange={nickNameEditonChangeHandler}
+              />
+              <button onClick={nickNameEditHandler}>닉네임수정</button>
+              <div>프로필소개</div>
+              <input
+                type="text"
+                placeholder="프로필 소개 들어감"
+                onChange={countOnChangeHandler}
+                value={Characters}
+              />
+              <p>{countCharacters}/50</p>
+              <div>
+                {' '}
+                <p> 프로필 정보를 성공적으로 변경하였습니다.</p>
+                <button onClick={confirmButtonHandler}>적용하기</button>
+              </div>
+            </div>
+          ) : (
+            <div className="PasswordEdit">
+              <div>
+                <p>현재 비밀번호</p>
+                <input type="text" />
+              </div>
+              <div>
+                <p>변경할 비밀번호</p>
+                <input type="text" />
+              </div>
+              <div>
+                <p>한번 더 입력해주세요 </p>
+                <input type="text" />
+              </div>
+              <div>
+                <p> 프로필 정보를 성공적으로 변경하였습니다.</p>
+                <button onClick={confirmButtonHandler}>적용하기</button>
+              </div>
+            </div>
+          )}
         </div>
       </div>
     </StyledEditProfileModalContainer>
