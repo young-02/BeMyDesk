77bb2e5db1f19ffe52b36825bb2ea75a85a97cc6
Jeong <hj08hn@gmail.com>
2023-02-21 14:20:14 +0900

    :sparkles:feat: add firebaseStorage #26
    - 파이어베이스 getStorage를 추가합니다

--- shared/firebase.ts
+++ shared/firebase.ts
@@ -2,6 +2,7 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -17,3 +18,4 @@ const firebaseConfig = {
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const dbService = getFirestore(app);
+export const storage = getStorage(app);
