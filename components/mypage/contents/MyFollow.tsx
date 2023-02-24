40502126105d84e1a27a4398f42df3f9ec220fff
HongguLim <Honggu.Lim@outlook.com>
2023-02-24 10:10:13 +0900

    💄: mypage CSS 작업

--- components/mypage/contents/MyFollow.tsx
+++ components/mypage/contents/MyFollow.tsx
@@ -20,19 +20,33 @@ function MyFollow({ myFollow, followCount }: any) {
     return (
       <>
         {myFollow.map((profile) => (
-          <div key={profile.id} style={{ border: '1px solid black' }}>
-            <div>
+          <StyledContainer
+            key={profile.id}
+            style={{ border: '1px solid black' }}
+          >
+            <StyledDivLeft>
               <img
+                className="profileImage"
                 src={profile.profileImage}
                 alt="Image"
                 width={202}
                 height={202}
               />
-            </div>
-            <p style={{ fontWeight: '700' }}>{profile.nickname}</p>
-            <p>{profile.introduction}</p>
-            <p>{profile.userId}</p>
-          </div>
+            </StyledDivLeft>
+            <StyledDivRight>
+              <div className="firstLine">
+                <button className="followingButton">
+                  <span className="followingButtonText">팔로잉</span>
+                </button>
+              </div>
+              <div className="secondLine">
+                <p className="nickname">{profile.nickname}</p>
+              </div>
+              <div className="thirdLine">
+                <p className="inroduction">{profile.introduction}</p>
+              </div>
+            </StyledDivRight>
+          </StyledContainer>
         ))}
       </>
     );
@@ -42,7 +56,110 @@ function MyFollow({ myFollow, followCount }: any) {
 const StyledContainer = styled.div`
   display: flex;
   width: 894px;
-  height: 197px;
+  height: 120px;
+  /* White */
+
+  background: #ffffff;
+  /* Primary 01 */
+
+  border: 1px solid #868e96;
+  border-radius: 10px;
+  margin-bottom: 28px;
+`;
+
+const StyledDivLeft = styled.div`
+  width: 160px;
+
+  .profileImage {
+    border-radius: 50%;
+    width: 80px;
+    height: 80px;
+    object-fit: cover;
+    margin: 20px 40px;
+  }
+`;
+
+const StyledDivRight = styled.div`
+  padding: 10px;
+  .firstLine {
+    display: flex;
+    justify-content: flex-end;
+    width: 700px;
+    .followingButton {
+      background: #e9ecef;
+      border-radius: 10px;
+      /* Pretendard Medium 14 */
+
+      font-family: 'Pretendard';
+      font-style: normal;
+      font-weight: 500;
+      font-size: 14px;
+      line-height: 20px;
+      /* identical to box height, or 143% */
+
+      display: flex;
+      align-items: center;
+      text-align: center;
+
+      /* Gray 06 */
+
+      color: #495057;
+      padding: 5px 22px;
+      .followingButtonText {
+        /* Pretendard Medium 14 */
+
+        font-family: 'Pretendard';
+        font-style: normal;
+        font-weight: 500;
+        font-size: 16px;
+        line-height: 20px;
+        /* identical to box height, or 143% */
+
+        display: flex;
+        align-items: center;
+        text-align: center;
+
+        /* Gray 06 */
+
+        color: #495057;
+      }
+    }
+  }
+  .secondLine {
+    .nickname {
+      /* Pretendard Bold 20 */
+
+      font-family: 'Pretendard';
+      font-style: normal;
+      font-weight: 700;
+      font-size: 20px;
+      line-height: 20px;
+      /* identical to box height, or 100% */
+
+      display: flex;
+      align-items: center;
+
+      /* Gray 09 */
+
+      color: #17171c;
+    }
+  }
+
+  .thirdLine {
+    margin-top: 10px;
+    .introduction {
+      font-family: 'Pretendard';
+      font-style: normal;
+      font-weight: 500;
+      font-size: 16px;
+      line-height: 20px;
+      /* identical to box height, or 125% */
+
+      /* Gray 09 */
+
+      color: #17171c;
+    }
+  }
 `;
 
 export default MyFollow;
