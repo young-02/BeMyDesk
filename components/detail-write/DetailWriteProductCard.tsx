8f5d09335a5bd3459dc7fffc6bbd6b4a26d507f7
Jeong <hj08hn@gmail.com>
2023-02-24 09:53:25 +0900

    :lipstick: Design: 모달창 UI 변경 #29
    - 모달창 안에 UI를 1차로 수정합니다
    - 모달창에서 선택한 제품들이 들어가는 각각의 카드들의 UI도 1차로 제작합니다

--- components/detail-write/DetailWriteProductCard.tsx
+++ components/detail-write/DetailWriteProductCard.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import styled from 'styled-components';
 import { DetailWriteSearchProps } from './DetailWriteForm';
 
+const parse = require('html-react-parser');
+
 type Props = {
   item?: string;
   list?: any[];
@@ -10,32 +12,60 @@ type Props = {
 // 모달창에서 선택한 제품이 들어가는 카드
 const DetailWriteProductCard = ({ selectList }: any) => {
   return (
-    <>
-      {/* <CardBox> */}
-
+    <div
+      style={{
+        display: 'flex',
+        overflowX: 'auto',
+        overflowY: 'hidden',
+      }}
+    >
       {selectList?.map((i: any) => (
         <CardLayout key={i.productId}>
           <CardBox>
-            <img src={i.image} style={{ width: '200px', height: '200px' }} />
+            <div
+              style={{
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}
+            >
+              <img
+                src={i.image}
+                style={{
+                  width: '150px',
+                  height: '150px',
+                }}
+              />
+              <div
+                style={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                }}
+              >
+                <p>{parse(i.title)}</p>
+                <p>{i.category2}</p>
+              </div>
+            </div>
           </CardBox>
         </CardLayout>
       ))}
-      {/* </CardBox> */}
-    </>
+    </div>
   );
 };
 
 const CardLayout = styled.div`
   display: flex;
-  justify-items: flex-start;
-  flex-direction: row;
-  align-items: center;
+  flex-direction: column;
 `;
 
 const CardBox = styled.div`
-  width: 17.625rem;
-  height: 10.625rem;
+  width: 31.25rem;
+  height: 31.25rem;
   border: 0.0625rem solid #868e96;
   border-radius: 0.625rem;
+  margin: 0.5rem;
 `;
 export default DetailWriteProductCard;
--- components/detail-write/DetailWriteSearch.tsx
+++ components/detail-write/DetailWriteSearch.tsx
@@ -3,6 +3,7 @@ import DetailWriteSearchModal from './DetailWriteSearchModal';
 import styled from 'styled-components';
 import axios from 'axios';
 import close from 'public/images/close.png';
+
 const parse = require('html-react-parser');
 
 axios.defaults.withCredentials = true;
@@ -44,14 +45,16 @@ const DetailWriteSearch = ({
           <DetailWriteSearchBox>
             <div>
               <span className="search">검색하기</span>
+              {/* <span className="search">직접 등록하기</span> */}
             </div>
             <button className="closeBtn" onClick={onClose}>
               <img className="closeBtnImage" src={close.src} />
             </button>
           </DetailWriteSearchBox>
-          <span>제품을 검색해주세요</span>
-          <div>
+          <p className="search_products">제품을 검색해주세요</p>
+          <SearchInputBox>
             <input
+              className="searchInput"
               id="products"
               type="text"
               placeholder="검색어를 입력해주세요"
@@ -59,12 +62,9 @@ const DetailWriteSearch = ({
               onChange={inputSearchWord}
             />
             <button onClick={getNaverData}>검색</button>
-          </div>
-          <div>
-            <span>제품을 선택해주세요</span>
-          </div>
+          </SearchInputBox>
         </DetailWriteSearchBoxTop>
-        <div>
+        <div style={{ padding: '1rem' }}>
           {data?.map((item: any) => (
             <DetailWriteSearchProductBox
               key={item.productId}
@@ -72,21 +72,39 @@ const DetailWriteSearch = ({
               onClick={() => selectProduct(item)}
             >
               <div className="searchProduct">
-                {parse(item.title)}
-                {item.category2}
+                <div style={{ width: '600px' }}>
+                  {item.title.split('<b>').join('').split('</b>').join('')}
+                </div>
+                <div
+                  style={{
+                    width: '80px',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                  }}
+                >
+                  {item.category2}
+                </div>
               </div>
             </DetailWriteSearchProductBox>
           ))}
         </div>
         <DetailWriteSearchBoxBottom>
           {list?.map((item: any) => (
-            <div key={item.productId}>
-              {parse(item.title)}
-              <button onClick={() => deleteProduct(item)}>삭제</button>
-            </div>
+            <PickProductsBox>
+              <div key={item.productId} className="pickProducts">
+                {parse(item.title)}
+              </div>
+              <div>
+                <button onClick={() => deleteProduct(item)}>
+                  <img className="closeBtn" src={close.src} />
+                </button>
+              </div>
+            </PickProductsBox>
           ))}
-          <button onClick={onClose}>닫기</button>
-          <button onClick={onClick}>등록하기</button>
+          <ButtonBox>
+            <button onClick={onClose}>닫기</button>
+            <button onClick={onClick}>등록하기</button>
+          </ButtonBox>
         </DetailWriteSearchBoxBottom>
       </DetailWriteSearchLayout>
     </DetailWriteSearchModal>
@@ -102,9 +120,16 @@ const DetailWriteSearchBox = styled.div`
   display: flex;
   flex-direction: row;
   align-items: center;
+  justify-content: space-between;
+  margin: 0.625rem;
+
+  > div {
+    display: flex;
+    align-items: center;
+  }
 
   .search {
-    font-size: 1.25rem;
+    font-size: 1rem;
     line-height: 1.25rem;
     margin: 0.5rem 1rem;
     cursor: pointer;
@@ -130,21 +155,25 @@ const DetailWriteSearchBox = styled.div`
 
 const DetailWriteSearchProductBox = styled.div<DetailWriteSearchProductBox>`
   display: flex;
-  /* overflow-y: scroll; */
 
   .searchProduct {
     display: flex;
+    justify-content: space-between;
     align-items: center;
     width: 46.875rem;
     height: 3.5rem;
-    font-size: 1.25rem;
+    font-size: 1rem;
     line-height: 1.25rem;
     border: 1px solid #868e96;
     padding: 1rem;
     margin: 0.625rem;
+
     border-radius: 0.625rem;
     overflow: hidden;
     cursor: pointer;
+  }
+  &:hover {
+    color: #206efb;
   }
 `;
 
@@ -152,12 +181,94 @@ const DetailWriteSearchBoxTop = styled.div`
   position: sticky;
   top: 0;
   background-color: #fff;
+
+  .search_products {
+    font-size: 22px;
+    font-weight: 700;
+    line-height: 3rem;
+    margin-bottom: 0.625rem;
+    padding-left: 1.25rem;
+  }
 `;
 
 const DetailWriteSearchBoxBottom = styled.div`
   position: sticky;
   bottom: 0;
+  height: 130px;
+  width: 100%;
   background-color: #fff;
+
+  .pickProducts {
+    color: #fff;
+    background-color: #206efb;
+    width: 200px;
+    height: 2.25rem;
+    margin: 0.625rem;
+    border-radius: 1.875rem;
+    padding: 0.5rem 1.25rem;
+    font-size: 1rem;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+  }
+`;
+
+const SearchInputBox = styled.div`
+  display: flex;
+  justify-content: space-around;
+  margin-bottom: 0.625rem;
+
+  > input {
+    width: 37.5rem;
+    height: 2.8125rem;
+    border-radius: 0.625rem;
+    border: 2px solid #868e96;
+    padding: 0 1rem;
+    font-size: 1rem;
+    line-height: 0.75rem;
+
+    &:focus {
+      border-color: #206efb;
+    }
+  }
+
+  > button {
+    width: 5.5rem;
+    height: 50px;
+    border: none;
+    border-radius: 0.625rem;
+    background-color: #206efb;
+    font-size: 16px;
+    color: #fff;
+    padding: 0.75rem;
+  }
+`;
+
+const PickProductsBox = styled.div`
+  display: flex;
+  flex-direction: row;
+
+  .closeBtn {
+    width: 10px;
+    height: 10px;
+  }
+`;
+
+const ButtonBox = styled.div`
+  display: flex;
+  justify-content: end;
+  /* background-color: #f1f3f5; */
+  /* width: 100%; */
+
+  > button {
+    border: none;
+    border-radius: 0.625rem;
+    background-color: #206efb;
+    color: #fff;
+    font-size: 1rem;
+    margin: 0.625rem;
+    padding: 0.5rem 1rem;
+  }
 `;
 
 export default memo(DetailWriteSearch);
