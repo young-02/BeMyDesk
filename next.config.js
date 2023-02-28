e142139df042a76590bce7613b408b78573f09e6
tyoung <taeyoung917@gmail.com>
2023-03-01 02:10:20 +0900

    🔨 Fix : img 도메인 추가

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-    images: {
-    domains: ['firebasestorage.googleapis.com'],
+  images: {
+    domains: ['firebasestorage.googleapis.com', 'shopping-phinf.pstatic.net'],
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
