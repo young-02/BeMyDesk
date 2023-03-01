e85f870c1b151703b7d34f7dfe4733027cf227a1
Jeong <hj08hn@gmail.com>
2023-03-01 15:14:57 +0900

    :sparkles:Feat: add swiper #28
    - 기기검색을 한 제품들이 슬라이드 형식으로 보이게 라이브러리를 적용합니다

--- components/detail-write/DetailWriteProductCard.tsx
+++ components/detail-write/DetailWriteProductCard.tsx
@@ -1,71 +1,117 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
-import { DetailWriteSearchProps } from './DetailWriteForm';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+import { Pagination, Navigation } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
 
 const parse = require('html-react-parser');
-
-type Props = {
-  item?: string;
-  list?: any[];
-};
 
 // 모달창에서 선택한 제품이 들어가는 카드
 const DetailWriteProductCard = ({ selectList }: any) => {
   return (
-    <div
-      style={{
-        display: 'flex',
-        overflowX: 'auto',
-        overflowY: 'hidden',
-      }}
-    >
-      {selectList?.map((i: any) => (
-        <CardLayout key={i.productId}>
-          <CardBox>
-            <div
-              style={{
-                display: 'flex',
-                flexDirection: 'column',
-                justifyContent: 'center',
-                alignItems: 'center',
-              }}
-            >
-              <img
-                src={i.image}
-                style={{
-                  width: '150px',
-                  height: '150px',
-                }}
-              />
-              <div
-                style={{
-                  display: 'flex',
-                  flexDirection: 'column',
-                  justifyContent: 'center',
-                  alignItems: 'center',
-                }}
-              >
-                <p>{parse(i.title)}</p>
-                <p>{i.category2}</p>
-              </div>
-            </div>
-          </CardBox>
-        </CardLayout>
-      ))}
-    </div>
+    <DetailWriteCardLayout>
+      <Swiper
+        slidesPerView={2}
+        centeredSlides={true}
+        spaceBetween={-65}
+        pagination={{
+          type: 'fraction',
+        }}
+        navigation={true}
+        modules={[Pagination, Navigation]}
+        className="mySwiper"
+      >
+        {selectList?.map((i: any) => (
+          <SwiperSlide>
+            {
+              <DetailWriteCardBox key={i.productId}>
+                <CardBox>
+                  <div className="card">
+                    <img
+                      src={i.image}
+                      style={{
+                        width: '200px',
+                        height: '200px',
+                        display: 'block',
+                        objectFit: 'cover',
+                      }}
+                    />
+                    <div className="card">
+                      <p className="product_title">{parse(i.title)}</p>
+                      <p className="product_hashTag">#{i.category2}</p>
+                    </div>
+                  </div>
+                </CardBox>
+              </DetailWriteCardBox>
+            }
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </DetailWriteCardLayout>
   );
 };
 
-const CardLayout = styled.div`
+const DetailWriteCardLayout = styled.div`
+  display: flex;
+  overflow-x: auto;
+  overflow-y: hidden;
+
+  .swiper {
+    width: 100%;
+    height: 100%;
+  }
+
+  .swiper-slide {
+    text-align: center;
+    font-size: 18px;
+    background: #fff;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+`;
+
+const DetailWriteCardBox = styled.div`
   display: flex;
   flex-direction: column;
 `;
 
 const CardBox = styled.div`
   width: 31.25rem;
-  height: 31.25rem;
   border: 0.0625rem solid #868e96;
   border-radius: 0.625rem;
-  margin: 0.5rem;
+  margin: 1.5rem;
+  padding: 1rem;
+
+  .card {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    justify-content: center;
+  }
+
+  .product_title {
+    font-size: 0.875rem;
+    font-weight: 700;
+    line-height: 1.25rem;
+    width: 30rem;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+  }
+
+  .product_hashTag {
+    color: #868e96;
+    font-size: 0.75rem;
+    line-height: 1rem;
+    font-weight: 500;
+    margin-top: 0.125rem;
+  }
 `;
+
 export default DetailWriteProductCard;
