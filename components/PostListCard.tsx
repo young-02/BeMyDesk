592d684ce83af2b6e3cd60af2abf6d8b36a74e36
Dayoung Park <qkrekdud26@gmail.com>
2023-03-01 18:13:15 +0900

    :recycle:Refactor: 시간 형태 변환 로직 utils 의 transDate 로 리팩토링 #15 #23 반복되는 시간 형태 변환 로직을 utils 함수로 리팩토링합니다.

--- components/PostListCard.tsx
+++ components/PostListCard.tsx
@@ -7,6 +7,7 @@ import { doc, updateDoc, query } from 'firebase/firestore';
 import { dbService } from '../shared/firebase';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
+import { transDate } from '@/utils/transDate';
 
 type PostListCardProps = { post: PostType; currentUserId?: string };
 
@@ -43,25 +44,7 @@ const PostListCard = ({ post, currentUserId }: PostListCardProps) => {
     }
   };
 
-  //시간경과
-  const detailDate = (time: any) => {
-    const milliSeconds = new Date() - time;
-    const seconds = milliSeconds / 1000;
-    if (seconds < 60) return `방금 전`;
-    const minutes = seconds / 60;
-    if (minutes < 60) return `${Math.floor(minutes)}분 전`;
-    const hours = minutes / 60;
-    if (hours < 24) return `${Math.floor(hours)}시간 전`;
-    const days = hours / 24;
-    if (days < 7) return `${Math.floor(days)}일 전`;
-    const weeks = days / 7;
-    if (weeks < 5) return `${Math.floor(weeks)}주 전`;
-    const months = days / 30;
-    if (months < 12) return `${Math.floor(months)}개월 전`;
-    const years = days / 365;
-    return `${Math.floor(years)}년 전`;
-  };
-  const nowDate = detailDate(post.createdAt);
+  const nowDate = transDate(post.createdAt);
 
   return (
     <PostListCardLayout key={post.id}>
