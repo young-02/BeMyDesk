fc97cd834cd2a9aab8cbfa485651aa8da418d235
Dayoung Park <qkrekdud26@gmail.com>
2023-03-02 21:49:05 +0900

    :recycle:Refactor: useUpdateLikes 컴포넌트별로 반복되는 요소 훅 안으로 삽입 #15 좋아요 +1 -1 업데이트될 객체를 훅 안으로 삽입합니다. 좋아요 클릭 여부 상태값을 훅 안으로 삽입합니다.

--- Hooks/useUpdateLikes.tsx
+++ Hooks/useUpdateLikes.tsx
@@ -1,44 +1,75 @@
 import { useMutation, useQueryClient } from 'react-query';
-import { dbService } from '@/shared/firebase';
+import { auth, dbService } from '@/shared/firebase';
 import { doc, updateDoc } from 'firebase/firestore';
+import { useState } from 'react';
+import { useRouter } from 'next/router';
 
-type UpdateLikesProp = { postId: string; newLikes: any };
-
-const updateLikes = ({ postId, newLikes }: UpdateLikesProp) => {
-  return updateDoc(doc(dbService, 'postData', postId), newLikes);
+type UpdateLikesProps = {
+  isLikesClicked: boolean;
+  post: PostType;
+  currentUserId?: any;
 };
 
-export const useUpdateLikes = () => {
+const updateLikes = async ({
+  isLikesClicked,
+  post,
+  currentUserId,
+}: UpdateLikesProps) => {
+  // 좋아요 +1
+  if (isLikesClicked === false) {
+    return updateDoc(doc(dbService, 'postData', post.id), {
+      likes: [...post.likes, currentUserId],
+      likesCount: post.likesCount + 1,
+    });
+  }
+  // 좋아요 -1
+  if (isLikesClicked === true) {
+    return updateDoc(doc(dbService, 'postData', post.id), {
+      likes: post.likes.filter((id) => id !== currentUserId),
+      likesCount: post.likesCount - 1,
+    });
+  }
+};
+
+export const useUpdateLikes = (currentUserId: any, post: PostType) => {
+  const initialState = post.likes.includes(currentUserId) ? true : false;
+  const [isLikesClicked, setIsLikesClicked] = useState(initialState);
   const queryClient = useQueryClient();
+  const router = useRouter();
+  const { query: currentQuery }: any = router;
 
-  return useMutation({
-    mutationFn: updateLikes,
-    onMutate: async ({ postId, newLikes }) => {
-      // 진행되는 모든 리패치들을 취소합니다.
-      await queryClient.cancelQueries({
-        queryKey: ['post-list', postId],
-      });
+  const { mutate: postListMutate } = useMutation(
+    () => updateLikes({ isLikesClicked, post, currentUserId }),
+    {
+      onMutate: async () => {
+        // 진행되는 모든 리패치들을 취소합니다.
+        await queryClient.cancelQueries({
+          queryKey: ['post-list', currentQuery],
+        });
 
-      // 기존 데이터를 snapshot 찍습니다.
-      const previousPost = queryClient.getQueryData(['post-list', postId]);
+        // 기존 데이터를 snapshot 찍습니다.
+        const previousPost = queryClient.getQueryData([
+          'post-list',
+          currentQuery,
+        ]);
+        console.log('previousPost', previousPost);
 
-      // console.log('연결은됨');
+        // // 성공을 가정하고 새로운 값으로 업데이트합니다.
+        // queryClient.setQueryData(['post-list'], newLikes);
 
-      // // 성공을 가정하고 새로운 값으로 업데이트합니다.
-      // queryClient.setQueryData(['post-list'], newLikes);
+        // 기존값과 새로 업데이트 된 값을 context 내부 값으로 반환합니다.
+        // return { previousPost };
+      },
 
-      // 기존값과 새로 업데이트 된 값을 context 내부 값으로 반환합니다.
-      return { previousPost, newLikes };
+      onError: () => {},
+
+      onSettled: () => {
+        // 좋아요 상태값을 변경하고, post-list를 refetching 합니다.
+        setIsLikesClicked(!isLikesClicked);
+        queryClient.invalidateQueries('post-list');
+      },
     },
+  );
 
-    // onError: () => {},
-
-    onSettled: (postId) => {
-      // queryClient.invalidateQueries({
-      //   queryKey: ['post-list', postId],
-      // });
-
-      queryClient.invalidateQueries('post-list');
-    },
-  });
+  return { isLikesClicked, postListMutate };
 };
