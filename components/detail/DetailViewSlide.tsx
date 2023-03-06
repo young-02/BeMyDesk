0e95237f2dfae2f8564a81acb4ef75c9c072741a
Jeong <hj08hn@gmail.com>
2023-03-06 17:49:48 +0900

    :lipstick: Design: UI 수정#28
    - 슬라이드 UI를 수정합니다

--- components/detail/DetailViewSlide.tsx
+++ components/detail/DetailViewSlide.tsx
@@ -26,10 +26,15 @@ export default function DetailViewSlide({ detail }) {
             objectFit="cover"
           />
         </SwiperSlide>
-        {/* <SwiperSlide>
-          <Image className="post-img" src={detail.postImage2} alt="postImage1"  layout="fill"
-            objectFit="cover"/>
-        </SwiperSlide> */}
+        <SwiperSlide>
+          <Image
+            className="post-img"
+            src={detail.postImage2}
+            alt="postImage1"
+            layout="fill"
+            objectFit="cover"
+          />
+        </SwiperSlide>
       </Swiper>
     </DetailViewSlideLayout>
   );
