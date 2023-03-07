1e7c9f62bd41ec247569c629f7488919b34dcd95
Jeong <hj08hn@gmail.com>
2023-03-08 00:26:23 +0900

    :hammer:Fix: change get url #40
    - 네이버 데이터 요청 url을 로컬호스트에서 배포한 사이트 url로 변경합니다

--- components/detail-write/DetailWriteForm.tsx
+++ components/detail-write/DetailWriteForm.tsx
@@ -80,7 +80,7 @@ const DetailWriteForm = ({ initialValues, mode }: any) => {
    */
   const getNaverData = async () => {
     const response = await axios
-      .get('http://localhost:3000/api/naverData', {
+      .get('https://be-my-desk-git-dev-young-02.vercel.app/api/naverData', {
         params: {
           query: searchWord,
         },
