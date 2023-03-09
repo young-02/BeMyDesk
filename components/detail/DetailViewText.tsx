b9559762bbf50357583854590ed24336527e5522
tyoung <taeyoung917@gmail.com>
2023-03-09 17:48:22 +0900

    :hammer:Fix: 카카오톡 공유하기 버튼 수정 #22

--- components/detail/DetailViewText.tsx
+++ components/detail/DetailViewText.tsx
@@ -4,7 +4,7 @@ import styled from 'styled-components';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 import ReactHtmlParser from 'html-react-parser';
 import { useGetKakao } from '@/Hooks/useGetKakao';
-import { kakaoInit } from '../../api.d';
+
 type Props = {};
 
 export default function DetailViewText({ post }) {
@@ -14,12 +14,15 @@ export default function DetailViewText({ post }) {
     setSocialSharing((prev) => !prev);
   };
   const currentUrl = window.location.href;
-  const kakaoShardUrl = `https://be-my-desk.vercel.app/detail/${post.id}`;
-  console.log(kakaoShardUrl);
 
   const status = useGetKakao('https://developers.kakao.com/sdk/js/kakao.js');
+
+  console.log(post);
+  // kakao sdk 초기화하기
+  // status가 변경될 때마다 실행되며, status가 ready일 때 초기화를 시도합니다.
   useEffect(() => {
     if (status === 'ready' && window.Kakao) {
+      // 중복 initialization 방지
       if (!window.Kakao.isInitialized()) {
         // 두번째 step 에서 가져온 javascript key 를 이용하여 initialize
         window.Kakao.init('a81b3af974c17b5fc4088249f10e7f77');
@@ -28,8 +31,27 @@ export default function DetailViewText({ post }) {
   }, [status]);
 
   const handleKakaoButton = () => {
-    window.Kakao.Link.sendScrap({
-      requestUrl: kakaoShardUrl,
+    const txt = post.postText.substr(2, 30);
+
+    window.Kakao.Link.sendDefault({
+      objectType: 'feed',
+      content: {
+        title: `${post.postTitle}`,
+        // description: txt,
+        imageUrl: `${post.postImage1}`,
+        link: {
+          mobileWebUrl: currentUrl,
+          androidExecParams: currentUrl,
+        },
+      },
+      buttons: [
+        {
+          title: '웹으로 이동',
+          link: {
+            mobileWebUrl: currentUrl,
+          },
+        },
+      ],
     });
   };
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,55 @@
+import React from 'react';
+import styled from 'styled-components';
+import { motion } from 'framer-motion';
+import gradient from '../../public/images/gradient.png';
+import Image from 'next/image';
+import MainSlide from '@/components/MainSlide';
 
-export default function Home() {
-  return <>Be my Desk Start</>;
+const item = {
+  inactive: { opacity: 0, translateY: 120 },
+  active: {
+    opacity: 1,
+    translateY: 0,
+    transition: { duration: 1, delay: 0.6, ease: 'easeInOut' },
+  },
+};
+
+export default function MainPage() {
+  return (
+    <MainPageLayout>
+      <MainSlide />
+      <Background>
+        <BackgroundGradient
+          className="gradient"
+          src={gradient}
+          alt="gradient-image"
+          width={2000}
+        />
+      </Background>
+    </MainPageLayout>
+  );
 }
+
+const MainPageLayout = styled.div`
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+  position: relative;
+`;
+
+const Background = styled.div`
+  z-index: -999;
+  position: fixed;
+  top: 0rem;
+  left: 0rem;
+  width: 100vw;
+  height: 100vh;
+  background-color: #17171c;
+`;
+
+const BackgroundGradient = styled(Image)`
+  z-index: -99;
+  position: fixed;
+  top: -10rem;
+  left: -14.5rem;
+`;
